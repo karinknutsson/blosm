3c6f1814e9607435e65ef04ef8a1ef936abc1594
Karin Knutsson <karinmiriam@gmail.com>
2026-02-02 18:50:25 +0100

    added dark mode toggle

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
+  <button @click="() => toggleDark()"><i class="pi pi-sun"></i></button>
   <router-view />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
 
 <style scoped></style>
