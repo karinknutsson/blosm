10d85c7c60f9e3be774b1f330fa297d09f107c74
Karin Knutsson <karinmiriam@gmail.com>
2026-02-02 10:27:55 +0100

    added primevue nora theme & commented out vue devtools

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
+import PrimeVue from "primevue/config";
+import Nora from "@primeuix/themes/nora";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Nora,
+  },
+});
 
-app.mount('#app')
+app.mount("#app");
